3191fbacb449cb6b548446dc22fc67fff822f957
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-03-25 17:04:58 -0300

    Adding toolbar and logout method

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -1,16 +1,61 @@
 package activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+import helpers.FirebaseConfig;
 import pedroadmn.instagramclone.com.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseConfig.getAuthFirebase();
+
+        Toolbar toolbar = findViewById(R.id.idToolbar);
+        toolbar.setTitle("Instagram");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logoutMenu:
+                logout();
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        try {
+            firebaseAuth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
